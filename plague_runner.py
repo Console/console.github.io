aae7e0fa04022d12c40430ee4d4ee9dbffaaf016
Console <Console@users.noreply.github.com>
2023-05-15 19:01:40 +0100

    Update plague_runner.py

--- plague_runner.py
+++ plague_runner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
